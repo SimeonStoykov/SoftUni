acffbc8c9e753f0db6611ee236d8aa6a69d87491
SimeonStoykov <simeon_stoykov@mail.bg>
2014-09-30 00:09:56 +0300

    Fixed bug

--- C# basics/Homeworks/1. Introduction to Programming/AgeIn10Years/AgeIn10Years.cs
+++ C# basics/Homeworks/1. Introduction to Programming/AgeIn10Years/AgeIn10Years.cs
@@ -6,6 +6,10 @@ class AgeIn10Years
     {
         DateTime birthday = DateTime.Parse(Console.ReadLine());
         int ageNow = DateTime.Now.Year - birthday.Year;
+        if ((DateTime.Now.Month < birthday.Month) || ((DateTime.Now.Month == birthday.Month) && (DateTime.Now.Date < birthday.Date)))
+        {
+            ageNow--;
+        }
         int ageIn10Years = ageNow + 10;
         Console.WriteLine("Your age now is: " + ageNow);
         Console.WriteLine("Your age in 10 years will be: " + ageIn10Years);
