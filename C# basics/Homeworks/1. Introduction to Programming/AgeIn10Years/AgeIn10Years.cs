bc7c86e84753901a25637631b2607cec02703efe
SimeonStoykov <simeon_stoykov@mail.bg>
2014-09-29 23:46:11 +0300

    Fixed task

--- C# basics/Homeworks/1. Introduction to Programming/AgeIn10Years/AgeIn10Years.cs
+++ C# basics/Homeworks/1. Introduction to Programming/AgeIn10Years/AgeIn10Years.cs
@@ -4,10 +4,10 @@ class AgeIn10Years
 {
     static void Main()
     {
-        int ageNow = int.Parse(Console.ReadLine());
+        DateTime birthday = DateTime.Parse(Console.ReadLine());
+        int ageNow = DateTime.Now.Year - birthday.Year;
         int ageIn10Years = ageNow + 10;
         Console.WriteLine("Your age now is: " + ageNow);
         Console.WriteLine("Your age in 10 years will be: " + ageIn10Years);
     }
 }
-
